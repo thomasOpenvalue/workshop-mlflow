1bfe4abf09eb813bda9ff3a56e34fcb8103b20a2
thomas.fontaine@openvalue.fr <thomas.fontaine@openvalue.fr>
2019-04-16 23:56:32 +0200

    refonte train.py

--- train.py
+++ train.py
@@ -10,9 +10,8 @@ import matplotlib
 matplotlib.use("TkAgg")
 from matplotlib import pyplot as plt
 from sklearn.linear_model import ElasticNet
-from sklearn.metrics import classification_report, mean_absolute_error, mean_squared_error, r2_score
-from sklearn.model_selection import train_test_split
-
+from sklearn.metrics import classification_report, mean_absolute_error, mean_squared_error, r2_score, make_scorer
+from sklearn.model_selection import train_test_split, cross_validate, cross_val_predict
 
 def load_data():
     df_all = pd.read_csv('./data/energydata_complete.csv')
@@ -41,17 +40,50 @@ def eval_metrics(actual, pred):
     return rmse, mae, r2
 
 
-def scatter_plot_result(y_actual, y_pred, plot_name):
+def cv_eval_metrics(cv_model):
+    rmse = np.sqrt(np.mean(cv_model['test_mse']))
+    mae = np.mean(cv_model['test_mae'])
+    r2 = np.mean(cv_model['test_r2'])
+    return rmse, mae, r2
+
+
+def scatter_plot_result(y_actual, y_pred, name):
     plt.scatter(y_actual, y_pred)
     plt.ylabel('Target predicted')
     plt.xlabel('True Target')
-    plt.title(plot_name)
-    plt.text(500, 250, r'$RMSE=%.2f, R^2$=%.2f, MAE=%.2f' % (np.sqrt(mean_squared_error(y_actual, y_pred)),
-                                                             r2_score(y_actual, y_pred),
-                                                             mean_absolute_error(y_actual, y_pred)))
-    plt.savefig(plot_name)
+    plt.title(name)
+    
+    pos_x = y_actual.max() * 0.60
+    pos_y = y_pred.max() * 0.90
+    
+    plt.text(pos_x, pos_y, r'$RMSE=%.2f, R^2$=%.2f, MAE=%.2f' % (np.sqrt(mean_squared_error(y_actual, y_pred)), 
+                                              r2_score(y_actual, y_pred), 
+                                              mean_absolute_error(y_actual, y_pred)))
+    plt.plot([0, y_actual.max()], [0, y_actual.max()], ls="--", c=".3")
+    plt.savefig('./scatter_results-{}.png'.format(name)) # Save to be included in Artefacts
     plt.close()
 
+def plot_cv_results(iMetric, iResCV, model_name=None):
+    # multiple line plot
+    plt.plot(range(1,6)
+             , iResCV["test_" + iMetric]
+             , marker='o'
+             , markerfacecolor='blue'
+             , markersize=12, color='skyblue'
+             , linewidth=4)
+    plt.plot(range(1,6)
+             , iResCV["train_" + iMetric]
+             , marker='o'
+             , markerfacecolor='red'
+             , markersize=12
+             , color='red'
+             , linewidth=4)
+    plt.xticks(range(1,6))
+    plt.legend(('test','train'))
+    plt.title(iMetric)
+    if model_name is not None:
+        plt.savefig('./cross_val_results-{}.png'.format(model_name))
+    plt.close()
 
 def log_metrics_classification(y_true, y_prediction):
     report = classification_report(y_true, y_prediction, output_dict=True)
@@ -61,8 +93,8 @@ def log_metrics_classification(y_true, y_prediction):
             # insert your code here ~ 1 line
 
 
-def log_metrics_regression(y_true, y_prediction):
-    rmse, mae, r2 = eval_metrics(y_true, y_prediction)
+def log_metrics_regression(cv_model):
+    rmse, mae, r2 = cv_eval_metrics(cv_model)
     # log metrics here ~ 3 lines
 
 
@@ -74,27 +106,43 @@ def set_mlfow_experiment(experiment_name):
 def run_experiment(df, alpha, l1_ration experiment_name=None):
     # set exeperiment here ~ 1 line
     
-    # Split data
-    train_x, train_y, test_x, test_y = get_train_test_data(df)
+     # Split features and targets
+    target_column = "Appliances"
+    y = df[target_column]
+    X = df.drop([target_column], axis=1)
 
+    # Nb Cross
+    CV = 5
+    
+    scorings = {
+        'mse' : make_scorer(mean_squared_error),
+        "mae" : make_scorer(mean_absolute_error),
+        "r2" : make_scorer(r2_score)
+    }
+    
     with mlflow.start_run():
         print("Running with alpha: {} - l1_ratio: {}".format(alpha, l1_ratio))
 
         # fit models
         lr = ElasticNet(random_state=0, alpha=alpha, l1_ratio=l1_ratio)
-        lr.fit(train_x, train_y)
+        model = cross_validate(lr, X, y, scoring=scorings, cv=CV, n_jobs=-1, return_train_score=True)
 
-        prediction_test = lr.predict(test_x)
+        prediction_test = cross_val_predict(lr, X, y, cv=CV, n_jobs=-1, verbose=1)
 
         # log parameters
         # Your code here ~ 2 lines
 
-        # log artifact
-        scatter_name = './scatter_results-ElasticNet.png'
-        # save scatter plot as artifact here ~ 2 lines
+        # log plots as artifacts
+        scatter_model_name = 'ElasticNet'
+        # save scatter plot as artifact here ~ 2 lines (1 to create the file, 1 to save as artifact)
+        
+        # save cv_results ~ 1 line
+        for metric in ['r2', 'mse', 'mae']:
+            cv_result_name = metric + '_cv_result_ElasticNet'
+            # plot cv result here using cv_result_name to save the plot then log as artifact ~ 2 line
 
         # log metrics
-        log_metrics_regression(test_y, prediction_test)
+        log_metrics_regression(model)
 
         # log sklearn model
         # log the sklearn model here  ~ 1 line
